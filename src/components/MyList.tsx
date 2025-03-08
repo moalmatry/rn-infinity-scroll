72cc487b9c9323b2b109143680cbb3adfe499bda
Moahmed Almarty <moalmatry2000@gmail.com>
2025-03-08 03:39:15 +0200

    start

--- src/components/MyList.tsx
+++
@@ -1,8 +0,0 @@
-import character from '../data/character.json';
-import CharacterListItem from './CharacterListItem';
-
-const MyList = () => {
-  return <CharacterListItem character={character.results[0]} />;
-};
-
-export default MyList;
